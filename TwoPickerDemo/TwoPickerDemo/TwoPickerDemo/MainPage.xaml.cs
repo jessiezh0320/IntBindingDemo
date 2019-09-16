927e6f00eb0da3e979dd2bcd96a2b19d50ce8361
nicole <nicole915@sina.com>
2019-09-16 20:16:14 +0800

    a binding demo

---
+++ TwoPickerDemo/TwoPickerDemo/TwoPickerDemo/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TwoPickerDemo
+{
+    public partial class MainPage : ContentPage
+    {
+
+        ObservableCollection<TestModel> items = new ObservableCollection<TestModel>();
+        MyViewModel testModel;
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //items = new ObservableCollection<TestModel>() {
+            //    new TestModel{ MyID = 1,ShowValue="test1" }, new TestModel{ MyID = 2,ShowValue="test2" },
+            //    new TestModel{ MyID = 3,ShowValue="test3" }, new TestModel{ MyID = 4,ShowValue="test4" }
+            //};
+
+            //mypicker.ItemsSource = items;
+
+
+            testModel = new MyViewModel();
+            BindingContext = testModel;
+
+        }
+
+
+        // to show the selected item
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Alert", "selected Item  MyID : " + testModel.SelectedRecord.MyID + "<--> ShowValue: " + testModel.SelectedRecord.ShowValue, "OK");
+        }
+
+    }
+}
---
+++ TwoPickerDemo/TwoPickerDemo/TwoPickerDemo/TestModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwoPickerDemo
+{
+    public  class TestModel
+    {
+        public int MyID { get; set; }
+
+        public string ShowValue { get; set; }
+    }
+}
